3925565249e354937936bf7b072467c0ca693c86
msi <msi@DESKTOP-Q39FBF1>
2023-04-12 19:14:08 +0300

    Добавьте файлы проекта.

---
+++ First_Project(helloworld).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    system("color E9");
+    setlocale(0, "UKR"); // для кириллицы
+    cout << "\n";
+    cout << "\t\t to be \n\n";
+    cout << "\t\t\t or not \n\n";
+    cout << "\t\t\t\t to be... \n\n";
+    cout << "\t\t\t\t\t /Shacspir/ \n\n";
+}
